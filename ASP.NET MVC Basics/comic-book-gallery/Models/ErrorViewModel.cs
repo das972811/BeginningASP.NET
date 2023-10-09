3d9a67389f1dae3caf6c49e8b9714cf22c1e3c0e
das972811 <salasdiego54@gmail.com>
2023-10-09 10:47:25 -0500

    Comic Book Gallery project setup

---
+++ ASP.NET MVC Basics/comic-book-gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace comic_book_gallery.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
