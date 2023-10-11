12f05bfdb542119b11970c5c666341104c904063
das972811 <salasdiego54@gmail.com>
2023-10-10 20:42:18 -0500

    Added a comic books detaled view

--- ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
+++ ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -6,11 +6,6 @@ public class ComicBooksController : Controller
 {
     public IActionResult Detail()
     {
-        if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-        {
-            return Redirect("/");
-        }
-
-        return Content("Hello from the comic books controller");
+        return View();
     }
 }
