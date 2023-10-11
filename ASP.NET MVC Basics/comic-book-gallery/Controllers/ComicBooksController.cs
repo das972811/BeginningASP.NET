aeafbfe5f2b013510700aafe3b250ac27dec6898
das972811 <salasdiego54@gmail.com>
2023-10-11 15:17:48 -0500

    Added model intropolation

--- ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
+++ ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -1,3 +1,4 @@
+using comic_book_gallery.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace comic_book_gallery.Controllers;
@@ -6,20 +7,35 @@ public class ComicBooksController : Controller
 {
     public IActionResult Detail()
     {
-        ViewBag.SeriesTitle = "The Amazing Spider-Man";
-        ViewBag.IssuedNumber = 700;
-        ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-        ViewBag.Artists = new string[]
+        var comicBook = new ComicBook()
         {
-            "Script: Dan Slott",
-            "Pencils: Humberto Ramos",
-            "Inks: Victor Olazaba",
-            "Colors: Edgar Delgado",
-            "Letters: Chris Eliopoulos"
+            SeriesTitle = "The Amazing Spier-Man",
+            IssueNumber = 700,
+            DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+            Artists = new Artist[]
+            {
+                new() { Name = "Script", Role = "Dan Slott" },
+                new() { Name = "Pendils", Role = "Humberto Ramos" },
+                new() { Name = "Inks", Role = "Victor Olazaba" },
+                new() { Name = "Colors", Role = "Edgar Delgado" },
+                new() { Name = "Letters", Role = "Chris Elipoulos" }
+            }
         };
 
+        // ViewBag.SeriesTitle = "The Amazing Spider-Man";
+        // ViewBag.IssuedNumber = 700;
+        // ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+        // ViewBag.Artists = new string[]
+        // {
+        //     "Script: Dan Slott",
+        //     "Pencils: Humberto Ramos",
+        //     "Inks: Victor Olazaba",
+        //     "Colors: Edgar Delgado",
+        //     "Letters: Chris Eliopoulos"
+        // };
 
 
-        return View();
+
+        return View(comicBook);
     }
 }
