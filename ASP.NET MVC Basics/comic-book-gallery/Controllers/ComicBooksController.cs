05dc99c71d68db30d4e04893ff9688689f18f74e
das972811 <salasdiego54@gmail.com>
2023-10-10 21:39:55 -0500

    Project setup with design files

--- ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
+++ ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -7,7 +7,7 @@ public class ComicBooksController : Controller
     public IActionResult Detail()
     {
         ViewBag.SeriesTitle = "The Amazing Spider-Man";
-        ViewBag.issuedNumber = 700;
+        ViewBag.IssuedNumber = 700;
         ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
         ViewBag.Artists = new string[]
         {
