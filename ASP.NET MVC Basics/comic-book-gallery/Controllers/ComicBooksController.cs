30474831e903fd584de30b2a8bea370839048e40
das972811 <salasdiego54@gmail.com>
2023-10-11 22:09:49 -0500

    Added data repository

--- ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
+++ ASP.NET MVC Basics/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -1,5 +1,6 @@
-using comic_book_gallery.Models;
 using Microsoft.AspNetCore.Mvc;
+
+using comic_book_gallery.Data;
 
 namespace comic_book_gallery.Controllers;
 
@@ -7,21 +8,7 @@ public class ComicBooksController : Controller
 {
     public IActionResult Detail()
     {
-        var comicBook = new ComicBook()
-        {
-            SeriesTitle = "The Amazing Spider-Man",
-            IssueNumber = 700,
-            DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-            Artists = new Artist[]
-            {
-                new() { Name = "Script", Role = "Dan Slott" },
-                new() { Name = "Pendils", Role = "Humberto Ramos" },
-                new() { Name = "Inks", Role = "Victor Olazaba" },
-                new() { Name = "Colors", Role = "Edgar Delgado" },
-                new() { Name = "Letters", Role = "Chris Elipoulos" }
-            }
-        };
-
-        return View(comicBook);
+        var comicBook = new ComicBookRepository();
+        return View(comicBook.GetComicBook(700));
     }
 }
---
+++ ASP.NET MVC Basics/comic-book-gallery/Data/ComicBookRepository.cs
@@ -0,0 +1,56 @@
+using System.ComponentModel;
+using comic_book_gallery.Models;
+
+namespace comic_book_gallery.Data;
+
+public class ComicBookRepository
+{
+    private static ComicBook[] _comicBooks = new ComicBook[]
+    {
+        new()
+        {
+            SeriesTitle = "The Amazing Spider-Man",
+            IssueNumber = 700,
+            DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+            Artists = new Artist[]
+            {
+                new() { Name = "Script", Role = "Dan Slott" },
+                new() { Name = "Pendils", Role = "Humberto Ramos" },
+                new() { Name = "Inks", Role = "Victor Olazaba" },
+                new() { Name = "Colors", Role = "Edgar Delgado" },
+                new() { Name = "Letters", Role = "Chris Elipoulos" }
+            }
+        },
+        new()
+        {
+            SeriesTitle = "The Amazing Spider-Man",
+            IssueNumber = 657,
+            DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+            Artists = new Artist[]
+            {
+                new() { Name = "Dan Slott", Role = "Script" },
+                new() { Name = "Marcos Martin", Role = "Pencils" },
+                new() { Name = "Marcos Martin", Role = "Inks" },
+                new() { Name = "Muntsa Vicente", Role = "Colors" },
+                new() { Name = "Joe Caramagna", Role = "Letters" }
+            },
+            Favorite = false
+        },
+        new()
+        {
+            SeriesTitle = "Bone",
+            IssueNumber = 50,
+            DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+            Artists = new Artist[]
+            {
+                new() { Name = "Jeff Smith", Role = "Script" },
+                new() { Name = "Jeff Smith", Role = "Pencils" },
+                new() { Name = "Jeff Smith", Role = "Inks" },
+                new() { Name = "Jeff Smith", Role = "Letters" }
+            },
+            Favorite = false
+        }
+    };
+
+    public ComicBook GetComicBook(int id) => _comicBooks.First(c => c.IssueNumber == id);
+}
